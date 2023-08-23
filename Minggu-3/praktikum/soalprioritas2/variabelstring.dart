478391c729236bffd2f9e79f99f6cc50cd76e0b1
Aditya Tri Prasetyo <johndoe@example.com>
2023-08-23 14:56:46 +0700

    fundamental dart

--- Minggu-3/praktikum/soalprioritas2/variabelstring.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String variabel1 = "Aditya";
-  String variabel2 = "Tri";
-  String variabel3 = "Prasetyo";
-
-  String gabunganString = variabel1 + " " + variabel2 + " " + variabel3;
-
-  print(gabunganString);
-}
