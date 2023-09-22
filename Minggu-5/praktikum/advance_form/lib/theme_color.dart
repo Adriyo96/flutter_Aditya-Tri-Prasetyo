eb2f30e285cea075245218005c2ed6e00d1a38cb
Aditya Tri Prasetyo <johndoe@example.com>
2023-09-22 22:28:47 +0700

    Minggu-5 Advance Form

---
+++ Minggu-5/praktikum/advance_form/lib/theme_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  Color headLine = const Color(0xFF1C1B1F);
+  Color supportingText = const Color(0xFF49454F);
+  Color buttonColor = const Color(0xFF6750A4);
+  Color initialAvatar = const Color(0xFF21005D);
+  Color avatarColor = const Color(0xFFEADDFF);
+  Color listColor = const Color(0xFFFFFBFE);
+  Color phoneIconColor = const Color(0xFF625B71);
+  Color borderColor = const Color(0xFFCAC4D0);
+  Color textFieldColor = const Color(0xFFE7E0EC);
+  Color labelTextColor = const Color(0xFFFFFFFF);
+}
